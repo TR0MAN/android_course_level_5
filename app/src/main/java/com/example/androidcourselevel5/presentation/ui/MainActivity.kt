93b81e0e8dc0658e7e285b73c517bd4643cdf3e7
Tkachenko Roman <githubpo4ta@gmail.com>
2024-08-18 22:41:49 +0300

    Level-5: Added authorization activity page

--- app/src/main/java/com/example/androidcourselevel5/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidcourselevel5/presentation/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.androidcourselevel5
+package com.example.androidcourselevel5.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidcourselevel5.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
