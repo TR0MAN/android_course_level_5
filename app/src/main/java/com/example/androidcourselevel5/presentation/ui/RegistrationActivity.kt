ee73c63fdc1ad2882e147b5b75b70c6105743ff0
Tkachenko Roman <githubpo4ta@gmail.com>
2024-08-19 23:35:46 +0300

    Level-5: Added registration activity page

---
+++ app/src/main/java/com/example/androidcourselevel5/presentation/ui/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidcourselevel5.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidcourselevel5.R
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
